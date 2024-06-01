f9f4fa8aafbc54f8ed28f1c45f2e3f31fd9ca267
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-06-01 19:48:09 +0530

    add: newupdate

--- src/compontets/Showvideo.jsx
+++ src/compontets/Showvideo.jsx
@@ -15,63 +15,40 @@ const Showvideo = () => {
             className="container-fluid mt-2"
             style={{ width: "100%", height: "auto", overflow: "auto" }}
           >
-            <div className='container'>
+            <div className='container-sm'>
                 <div className="row">
                     <div className="col-sm-8">
-                    <video src={videoFile} autoPlay controls width={'100%'} style={{borderRadius:'20px'}}></video>
-
+                    <video src={videoFile} autoPlay controls width={'100%'} style={{borderRadius:'10px'}}></video>
                     <div className="container">
                       <div className="row">
                         <div className="col-sm-12">
                          <h6 className='text-sm'>{findData.VideoTitle}</h6>
                           </div>
-                          <div className='col-5 '>
+                          <div className='col-12 text-end'>
+                          <MDBIcon fas icon="heart" className='mx-2'/>
+                        <MDBBtn color='white' className=' mx-3' size='sm' style={{borderRadius:"20px"}}>  <MDBIcon fas icon="user-plus" /> follow</MDBBtn> 
+                        <MDBIcon fas icon="ellipsis-v" />
+                          </div>
+                          <div className='col-12 p-0'>
                           <img  className="img-sm-fluid rounded-circle" src={findData.ProFile}   alt="profile" width={"35px"}/>
-                          <p  className="text-sm-start" style={{display:'inline'}}> <b>{findData.ChannelName}</b> </p>                          
+                          <p  className="text-sm-start" style={{display:'inline'}}> <b>{findData.ChannelName}</b> </p>     
+                          <pre className='sm-p-2' style={{display:'inline'}}>1.3M follewers</pre>                     
                           </div>
-                          <div className='col-7'>
-                          <div className="container-fluid text-end"> 
-                          <pre className='sm-p-2' style={{display:'inline'}}>1.3M follewers</pre>
-                          <MDBBtn color='white' className='sm ' size='sm' style={{borderRadius:"20px"}}>  <MDBIcon fas icon="user-plus" /> follow</MDBBtn> 
-                          <MDBBtn color='white' className='sm' size='sm' style={{borderRadius:"20px"}}>  <MDBIcon far icon="heart"/></MDBBtn> 
-                          <MDBBtn color='white' className='sm p-0' size='sm' style={{borderRadius:"20px"}}>  <MDBIcon fas icon="ellipsis-v" /></MDBBtn> 
-                          </div>                          
+                          
                           </div>
                           </div>
-                          </div>
-
-                    {/* <div className="container-sm-fluid"> */}
-                    {/* <h5>{findData.VideoTitle}</h5> */}
-                    {/* <img
-                      className="img-fluid rounded-circle mb-2"
-                      src={findData.ProFile}
-                      alt="profile"
-                      width={"30px"}
-                    /> */}
-                    {/* <p  className="text-sm-start p-2" style={{display:'inline'}}> <b>{findData.ChannelName}</b> </p> */}
-                    {/* <pre className='sm-p-2' style={{display:'inline'}}>1.3M follewers</pre> */}
-                    {/* <div className="container bg-black"  style={{display:'inline'}}> */}
-                    {/* <MDBIcon className='mx-3' far icon="heart"/>
-                      */}
-                    {/* <MDBBtn rounded  color='dark' className='sm' size='sm' style={{borderRadius:"20px"}}>  <MDBIcon fas icon="user-plus" /> unfollow</MDBBtn>   */}
-                    {/* <MDBIcon fas icon="user-plus" size='1xl'/>  */}
-                                   
-                    {/* <MDBIcon fas icon="ellipsis-v" /> */}
-                    {/* </div> */}                    
-                    {/* </div> */}
                     </div>
                     <div className="col-sm-4">
-                        <div className="container"  style={{ width: "100%",height:"600px", overflow: "auto", borderRadius:'10px', backgroundColor:'#ede8e8'}} >
+                        <div className="container-fluid"  style={{ width: "100%",height:"600px", overflow: "auto", borderRadius:'10px', backgroundColor:'white'}} >
                         {data.map((item) => (    
                                       <div
-                className="container "
-                style={{ width: "300px", height: "auto" }}                
+                className="container-fluid p-1"
+                style={{ width: "100%", height: "auto" }}                
               >
                 <img
                   src={item.Thumbnail}
                   alt="Thumbnil"
                   width={"100%"}
-                  height={"90%"}
                 />
 
                 <div class="d-flex flex-row bd-highlight mb-0 p-0">
